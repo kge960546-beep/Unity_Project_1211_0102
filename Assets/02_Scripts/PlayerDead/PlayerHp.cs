6c81c46c4ab7f6e8f75ca46bd515cd6f9bce6f1b
gueon <kge9605@naver.com>
2025-12-11 21:18:47 +0900

    fix: Prevent external modification of events

--- Assets/02_Scripts/PlayerDead/PlayerHp.cs
+++ Assets/02_Scripts/PlayerDead/PlayerHp.cs
@@ -6,20 +6,47 @@ public class PlayerHp : MonoBehaviour
     /// <summary>
     /// onPlayerDeadEvent: 플레이어가 죽었을 때 발생하는 이벤트
     /// </summary>
-    public static event Action onPlayerDeadEvent;
+    private static event Action onPlayerDeadEvent;
     /// <summary>
     /// onTakeDamage: 데미지를 입었을 때 발생하는 이벤트
     /// int: 입은 데미지 양 (finalDamage)
     /// int: 남은 체력 (currentHp)
     /// </summary>
-    public event Action<int, int> onTakeDamage;
+    private event Action<int, int> onTakeDamageEvent;
 
     [SerializeField] private int maxHp = 100;
     private int currentHp;
     private bool isDead = false;
 
     private Animator anim;
-
+    #region DeathSubscriptionAndCancellation
+    /// <summary>
+    /// 데드 이벤트 구독 & 해지
+    /// </summary>
+    /// <param name="action"></param>
+    public void SubscribePlayerDeadEvent(Action action)
+    {
+        onPlayerDeadEvent += action;
+    }
+    public void UnsubscribePlayerDeadEvent(Action action)
+    {
+        onPlayerDeadEvent -= action;
+    }
+    #endregion
+    #region TakeDamageSubscriptionAndCancellation
+    /// <summary>
+    /// 피격 이벤트 구독 & 해지
+    /// </summary>
+    /// <param name="action"></param>
+    public void SubscriptionPlayerTakeDamageEvent(Action<int, int> action)
+    {
+        onTakeDamageEvent += action;
+    }
+    public void UnsubscriptionPlayerTakeDamageEvent(Action<int, int> action)
+    {
+        onTakeDamageEvent -= action;
+    }
+    #endregion
     private void Awake()
     {
         currentHp = maxHp;
@@ -33,9 +60,9 @@ public class PlayerHp : MonoBehaviour
         int finalDamage = Mathf.Max(damage, 0);
 
         currentHp = Mathf.Max(currentHp - finalDamage, 0);
-        //히트 이펙트 있으면 여기서 재생        
+        // TODO: 히트 이펙트 있으면 여기서 재생        
 
-        onTakeDamage?.Invoke(finalDamage, currentHp);
+        onTakeDamageEvent?.Invoke(finalDamage, currentHp);
 
         if (currentHp <= 0) { Die(); }
     }
@@ -49,12 +76,14 @@ public class PlayerHp : MonoBehaviour
 
         onPlayerDeadEvent?.Invoke();
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(isDead) return;
-    }
-
-    //event를 외부에 직접 public으로 노출하는 건 외부에서 대입을 해버릴 수 있기에 위험함.
-    //Subscribe ~/Unsubscribe ~메소드를 통하여 간접적으로 등록/해제를 관리하는 것이 권장됨.
+        LayerMask.GetMask("Enemy");
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            // TODO: 데미지 적 정보로 불러오기 임시로 20 데미지
+            TakeDamage(20);
+        }
+    }    
 }
