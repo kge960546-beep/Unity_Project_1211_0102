266bf7651cfe1d62523fcabe4581ec7b43a8ec5e
gueon <kge9605@naver.com>
2025-12-11 16:10:36 +0900

    feat: Monster death event and Monster hit and damage calculation  #14 , #13
    I implemented a death judgment when the health reaches 0 using the delegate action and observer pattern, and I implemented damage calculation, but I have not implemented collision handling yet.

--- Assets/02_Scripts/EnemyDead/EnemyDead.cs
+++ Assets/02_Scripts/EnemyDead/EnemyDead.cs
@@ -1,4 +1,64 @@
-public interface IEnemyDead //적이 죽었을때 무엇을 할지 정의하는 인터페이스
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDead : MonoBehaviour
 {
-    void OnEnemyDead();
+    [Header("Prefab")]
+    [SerializeField] private GameObject expPrefab;
+    [SerializeField] private GameObject skillRandomBoxPrefab;
+    [SerializeField] private GameObject[] expendablesPrefabs;
+
+    [Header("UI")]
+    [SerializeField] private GameObject clearPanel;    
+
+    private void OnEnable()
+    {      
+        EnemyHp.onEnemyDeadEvent += OnEnemy;
+    }
+    private void OnDisable()
+    {
+        EnemyHp.onEnemyDeadEvent -= OnEnemy;
+    }
+    private void OnEnemy(EnemyType type, Vector3 deadPos, int expDropAmnount)
+    {
+        if(type == EnemyType.Boss)
+        {
+            //시간대 별로 보스 드랍 On, Off 가능하게 변경 필요
+            //마지막 보스는 드랍x
+            if (clearPanel != null) { clearPanel.SetActive(true); }
+
+            if(expendablesPrefabs != null)
+            {
+                foreach (GameObject item in expendablesPrefabs)
+                {
+                    SpawnItem(item, deadPos, 1);
+                }
+            }
+            
+            SpawnItem(skillRandomBoxPrefab, deadPos, 1);
+        }
+
+        if(type == EnemyType.Elite)
+        {            
+            SpawnItem(skillRandomBoxPrefab, deadPos, 1);
+        }
+
+        SpawnItem(expPrefab, deadPos, expDropAmnount);
+    }   
+    private void SpawnItem(GameObject prefab, Vector3 centerPos, int count)
+    {
+        if(prefab == null) return;
+
+        for (int i = 0; i < count; i++)
+        {
+            Vector2 offset = UnityEngine.Random.insideUnitCircle * 0.5f;
+
+            Vector3 spawnPos = centerPos + (Vector3)offset;
+
+            Instantiate(prefab, centerPos + (Vector3)offset, Quaternion.identity);
+        }
+    }
+
 }
--- Assets/02_Scripts/EnemyDead/EnemyHp.cs
+++ Assets/02_Scripts/EnemyDead/EnemyHp.cs
@@ -1,36 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum EnemyType
+{
+    Normal,
+    Elite,
+    Boss
+}
 public class EnemyHp : MonoBehaviour
 {
+    public static event Action<EnemyType, Vector3, int> onEnemyDeadEvent; //적이 죽었을때 적 타입과 위치를 전달
+
+    [Header("Enemy Setting")]
+    [SerializeField] private EnemyType enemyType = EnemyType.Normal; //적 타입
     [SerializeField] private int maxHp = 100;
+    [SerializeField] private int dropExp = 1;
+    
     private int currentHp;
+    private bool isDead = false;       
+
     private Animator anim;
-    private EnemyDeadSubject deadSubject;
-    private bool isDead = false;
+
     private void Awake()
     {
         currentHp = maxHp;
+
         anim = GetComponent<Animator>();
-        deadSubject = GetComponent<EnemyDeadSubject>();
+
     }
     public void TakeDamage(int damage)
     {
         if (isDead) return;
-        currentHp -= damage;
-        if (currentHp <= 0)
-        {
-            currentHp = 0;
-            Die();
+
+        currentHp = Mathf.Max(currentHp - damage, 0);
+        //히트 이펙트 있으면 여기서 재생             
+
+        if (currentHp <= 0) 
+        {           
+            Die(); 
         }
     }
     private void Die()
     {
-        if(isDead) return;
+        if (isDead) return;
+
         isDead = true;
-        anim.SetTrigger("Dead");
-        if(deadSubject != null)
-            deadSubject.NotifyEnemyDead();
+
+        if (anim != null)
+            anim.SetTrigger("isDead");
+
+        onEnemyDeadEvent?.Invoke(enemyType, transform.position, dropExp);
+
+        StartCoroutine(DeadTime());
+    }
+    IEnumerator DeadTime()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
     }
 }
