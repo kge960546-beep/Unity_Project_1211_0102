782f9e0d23d076343214046d9224cad529f761f6
gueon <kge9605@naver.com>
2025-12-12 16:10:54 +0900

    fix: Implement player death check and damage handling
    Unified event static and added null prevention logic.

--- Assets/02_Scripts/PlayerDead/GameOverUI.cs
+++ Assets/02_Scripts/PlayerDead/GameOverUI.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverUI;
-    PlayerHp playerHp;
+    [SerializeField] private PlayerHp playerHp;
 
+
+    private void Awake()
+    {
+        if(playerHp == null)
+            playerHp = FindObjectOfType<PlayerHp>();
+
+        if(gameOverUI != null)
+            gameOverUI.SetActive(false);
+    }
+
+    //TODO: 부활 기능을 넣을려면 클래스 이름변경과, 새로운 구독 해지 생성
+    /// <summary>
+    /// 플레이어가 게임에서 완전사망시 실패 패널로 넘어기는 구독 / 해지
+    /// </summary>    
     private void OnEnable()
     {
-        playerHp.SubscribePlayerDeadEvent(OnPlayerDead);
+        if(playerHp != null)
+            playerHp.SubscribePlayerDeadEvent(OnPlayerDead);
     }
     private void OnDisable()
     {
-        playerHp.UnsubscribePlayerDeadEvent(OnPlayerDead);
+        if (playerHp != null)
+            playerHp.UnsubscribePlayerDeadEvent(OnPlayerDead);
     }
     private void OnPlayerDead()
     {
--- Assets/02_Scripts/PlayerDead/PlayerHp.cs
+++ Assets/02_Scripts/PlayerDead/PlayerHp.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerHp : MonoBehaviour
@@ -6,7 +7,7 @@ public class PlayerHp : MonoBehaviour
     /// <summary>
     /// onPlayerDeadEvent: 플레이어가 죽었을 때 발생하는 이벤트
     /// </summary>
-    private static event Action onPlayerDeadEvent;
+    private event Action onPlayerDeadEvent;
     /// <summary>
     /// onTakeDamage: 데미지를 입었을 때 발생하는 이벤트
     /// int: 입은 데미지 양 (finalDamage)
@@ -19,6 +20,7 @@ public class PlayerHp : MonoBehaviour
     private bool isDead = false;
 
     private Animator anim;
+    private Rigidbody2D rb;
     #region DeathSubscriptionAndCancellation
     /// <summary>
     /// 데드 이벤트 구독 & 해지
@@ -38,11 +40,11 @@ public class PlayerHp : MonoBehaviour
     /// 피격 이벤트 구독 & 해지
     /// </summary>
     /// <param name="action"></param>
-    public void SubscriptionPlayerTakeDamageEvent(Action<int, int> action)
+    public void SubscribePlayerTakeDamageEvent(Action<int, int> action)
     {
         onTakeDamageEvent += action;
     }
-    public void UnsubscriptionPlayerTakeDamageEvent(Action<int, int> action)
+    public void UnsubscribePlayerTakeDamageEvent(Action<int, int> action)
     {
         onTakeDamageEvent -= action;
     }
@@ -51,7 +53,8 @@ public class PlayerHp : MonoBehaviour
     {
         currentHp = maxHp;
 
-        anim = GetComponent<Animator>();        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
     public void TakeDamage(int damage)
     {
@@ -75,11 +78,15 @@ public class PlayerHp : MonoBehaviour
             anim.SetTrigger("isDead");
 
         onPlayerDeadEvent?.Invoke();
+        rb.bodyType = RigidbodyType2D.Static;
+        //TODO: 플레이어 컨트롤러에서 PlayerHp를 불러와 구독 해지 추가
+        
     }
+   
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(isDead) return;
-        LayerMask.GetMask("Enemy");
+        
         if(collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             // TODO: 데미지 적 정보로 불러오기 임시로 20 데미지
