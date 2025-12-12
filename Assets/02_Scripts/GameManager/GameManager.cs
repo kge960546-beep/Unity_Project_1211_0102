ac30641cc61d919c3315e6d16ad6ea392dfca5a4
Seungyoon Lee <webmaster@cygnus.stream>
2025-12-12 10:59:57 +0900

    feat: add game manager basic design
    The GameManager works as a singleton, and its services are separated into parts. Service classes can be accessed using their types as keys.

---
+++ Assets/02_Scripts/GameManager/GameManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Singleton game manager.
+/// Services are separated into parts and can be accessed using their types as keys.
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    [Header("Service")]
+    [Tooltip("List of 'service' MonoBehaviours to be created for globally unique 'singleton' GameObject. Requires to extend IGameManagementService.")]
+    [SerializeField] private List<IGameManagementService> serviceDefinitions;
+    public Dictionary<Type, IGameManagementService> registeredServices { get; private set; }
+
+    [Header("Misc")]
+    [Tooltip("Base event system for UGUI to be converted into a singleton object. Required for additive scene loading.")]
+    // should preserve only one unique event system to enable additive scene loading
+    [SerializeField] private GameObject eventSystem;
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (null != Instance && this != Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+        if (null != eventSystem) DontDestroyOnLoad(eventSystem);
+
+        foreach (IGameManagementService svc in serviceDefinitions)
+        {
+            if (svc is not MonoBehaviour) continue;
+            registeredServices.Add(svc.GetType(), gameObject.AddComponent(svc.GetType()) as IGameManagementService);
+        }
+    }
+
+    public T GetService<T>() where T : class, IGameManagementService
+    {
+        return registeredServices[typeof(T)] as T;
+    }
+}
