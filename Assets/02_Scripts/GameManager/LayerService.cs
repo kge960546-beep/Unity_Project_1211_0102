65ac161005aa997d052a3047988fadc5cb987a43
Seungyoon Lee <webmaster@cygnus.stream>
2025-12-12 11:01:35 +0900

    feat: add layer cache and in-game timer service

---
+++ Assets/02_Scripts/GameManager/LayerService.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Layer cache
+/// </summary>
+public class LayerService : MonoBehaviour, IGameManagementService
+{
+    public int playerLayer { get; private set; }
+    public int enemyLayer { get; private set; }
+    public int bossLayer { get; private set; }
+    public int playerProjectileLayer { get; private set; }
+    public int enemyProjectileLayer { get; private set; }
+    public int groundLayer { get; private set; }
+    public int destructibleItemLayer { get; private set; }
+    public int indestructibleItemLayer { get; private set; }
+
+    private void OnEnable()
+    {
+        playerLayer = LayerMask.NameToLayer("Player");
+        enemyLayer = LayerMask.NameToLayer("Enemy");
+        bossLayer = LayerMask.NameToLayer("Boss");
+        playerProjectileLayer = LayerMask.NameToLayer("PlayerProjectile");
+        enemyProjectileLayer = LayerMask.NameToLayer("EnemyProjectile");
+        groundLayer = LayerMask.NameToLayer("Ground");
+        destructibleItemLayer = LayerMask.NameToLayer("DestructibleItem");
+        indestructibleItemLayer = LayerMask.NameToLayer("IndestructibleItem");
+    }
+}
