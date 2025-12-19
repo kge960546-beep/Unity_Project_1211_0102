bea05f6d3aa1192aa6b739e5e7470350a92a28d0
gueon <kge9605@naver.com>
2025-12-19 10:46:33 +0900

    fix: ground Layer Changed "Ground" and Create KillCount Script

---
+++ Assets/02_Scripts/UI/KillCount.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KillCount : MonoBehaviour
+{
+    private TextMeshProUGUI killCount;
+
+    private int currentKillCount = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
