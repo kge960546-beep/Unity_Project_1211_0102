44d5c27a916faf7c801ea604dd30420b0fffafb7
Seungyoon Lee <webmaster@cygnus.stream>
2025-12-19 19:12:24 +0900

    fix: defer projectile behaviour after character move

--- Assets/02_Scripts/Projectiles/ProjectileLogicRunner.cs
+++ Assets/02_Scripts/Projectiles/ProjectileLogicRunner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DefaultExecutionOrder(10)]
 public class ProjectileLogicRunner : MonoBehaviour
 {
     public ProjectileLogicBase Logic { set; private get; }
