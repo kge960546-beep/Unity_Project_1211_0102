305ae0c2260eed4869206065dee87efc2eadc9f6
gueon <kge9605@naver.com>
2025-12-27 01:48:23 +0900

    feat: add Creating a UI for merging equipment inventories

---
+++ Assets/02_Scripts/MargeInventory/InventoryManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public Transform rootSlot;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
