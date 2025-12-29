21ce7e682d78cf42293037b3efa38963222ca024
Seungyoon Lee <webmaster@cygnus.stream>
2025-12-29 10:30:42 +0900

    feat: introduce randomized projectile damage

--- Assets/02_Scripts/Projectiles/ProjectileCollisionDamageBehaviour.cs
+++ Assets/02_Scripts/Projectiles/ProjectileCollisionDamageBehaviour.cs
@@ -6,6 +6,7 @@ public class ProjectileCollisionDamageBehaviour : MonoBehaviour
     public float CriticalRate { set; private get; }
     public int OrdinaryDamage { set; private get; }
     public int CriticalDamage { set; private get; }
+    [Tooltip("Random value with uniform distribution from (1 - ratio) to (1 + ratio)")] public int DamageVariationRatio { set; private get; }
 
     public struct Cooldown
     {
@@ -42,7 +43,9 @@ public class ProjectileCollisionDamageBehaviour : MonoBehaviour
     {
         RandomService rs = GameManager.Instance.GetService<RandomService>();
         bool isCritical = rs.random.NextFloat(0f, 1f) < CriticalRate;
+        float randomRatio = GameManager.Instance.GetService<RandomService>().random.NextFloat(1f - DamageVariationRatio, 1f + DamageVariationRatio);
         int damage = isCritical ? CriticalDamage : OrdinaryDamage;
+        damage = (int)(damage * randomRatio);
         GameManager.Instance.GetService<DamageManagementService>().QueueDamage(damage, gameObject, target, isCritical);
     }
 }
